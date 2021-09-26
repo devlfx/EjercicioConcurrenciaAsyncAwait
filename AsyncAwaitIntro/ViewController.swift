122370932bb18e6f7ce8c3fdf17cdd7dfb75c54c
abe <laog18@gmail.com>
2021-09-26 16:46:16 -0500

    We add the methods modified to use dispatch queues, we define a method thaht simulates a synchronus request

--- AsyncAwaitIntro/ViewController.swift
+++ AsyncAwaitIntro/ViewController.swift
@@ -95,6 +95,63 @@ func downloadImageAndMetadata(
     imageTask.resume()
 }
 
+
+
+
+
+// MARK: Ejercicio reescritura de método empleando colas como lo vimos en clase.
+func downloadImageAndMetadataWithQueue(imageNumber: Int, qos: DispatchQoS.QoSClass = .background,
+                                       completionHandler: @escaping (_ image: DetailedImage?, _ error: Error?) -> Void){
+    let imageUrl = URL(string: "https://www.andyibanez.com/fairesepages.github.io/tutorials/async-await/part1/\(imageNumber).png")!
+    let metadataUrl = URL(string: "https://www.andyibanez.com/fairesepages.github.io/tutorials/async-await/part1/\(imageNumber).json")!
+    // Empleamos las colas con la prioridad ingresada por defecto es backgroud
+    // Debido a que empleamos el método sincrono definido en la extensión esto simula lo que hace
+    // dataTask por si mismo.
+    DispatchQueue.global(qos: qos).async {
+        let (dataImage,responseImage,errorImage) = URLSession.shared.syncDataTask(url: imageUrl)
+        guard let data = dataImage, let image = UIImage(data: data), (responseImage as? HTTPURLResponse)?.statusCode == 200 else {
+            completionHandler(nil, ImageDownloadError.badImage)
+            return
+        }
+        DispatchQueue.global(qos: qos).async{
+            let (metadataImage,metadataResponse,metadataError) = URLSession.shared.syncDataTask(url: metadataUrl)
+            
+            guard let mData = metadataImage, let metadata = try? JSONDecoder().decode(ImageMetadata.self, from: mData),  (metadataResponse as? HTTPURLResponse)?.statusCode == 200 else {
+                completionHandler(nil, ImageDownloadError.invalidMetadata)
+                return
+            }
+            
+            let detailedImage = DetailedImage(image: image, metadata: metadata)
+            completionHandler(detailedImage, nil)
+        }
+        
+    }
+    
+}
+
+func downloadImageAndMetadataWithQueue2(imageNumber: Int, qos: DispatchQoS.QoSClass = .background,
+                                       completionHandler: @escaping (_ image: DetailedImage?, _ error: Error?) -> Void){
+        let imageUrl = URL(string: "https://www.andyibanez.com/fairesepages.github.io/tutorials/async-await/part1/\(imageNumber).png")!
+        let metadataUrl = URL(string: "https://www.andyibanez.com/fairesepages.github.io/tutorials/async-await/part1/\(imageNumber).json")!
+
+    DispatchQueue.global(qos: qos).async {
+        let (dataImage,responseImage,errorImage) = URLSession.shared.syncDataTask(url: imageUrl)
+        guard let data = dataImage, let image = UIImage(data: data), (responseImage as? HTTPURLResponse)?.statusCode == 200 else {
+            completionHandler(nil, ImageDownloadError.badImage)
+            return
+        }
+        let (metadataImage,metadataResponse,metadataError) = URLSession.shared.syncDataTask(url: metadataUrl)
+        guard let mData = metadataImage, let metadata = try? JSONDecoder().decode(ImageMetadata.self, from: mData),  (metadataResponse as? HTTPURLResponse)?.statusCode == 200 else {
+            completionHandler(nil, ImageDownloadError.invalidMetadata)
+            return
+        }
+        let detailedImage = DetailedImage(image: image, metadata: metadata)
+        completionHandler(detailedImage, nil)
+    }
+}
+
+
+
 // MARK: - Main class
 
 class ViewController: UIViewController {
@@ -113,13 +170,13 @@ class ViewController: UIViewController {
         
         // MARK: METHOD 1 - Using Async/Await
         
-        Task {
-            if let imageDetail = try? await downloadImageAndMetadata(imageNumber: 1) {
-                self.imageView.image = imageDetail.image
-                self.metadata.text = "\(imageDetail.metadata.name) (\(imageDetail.metadata.firstAppearance) - \(imageDetail.metadata.year))"
-            }
-        }
-        
+//        Task {
+//            if let imageDetail = try? await downloadImageAndMetadata(imageNumber: 1) {
+//                self.imageView.image = imageDetail.image
+//                self.metadata.text = "\(imageDetail.metadata.name) (\(imageDetail.metadata.firstAppearance) - \(imageDetail.metadata.year))"
+//            }
+//        }
+//
         // MARK: METHOD 2 - Using async properties
         
 //        async {
@@ -142,6 +199,55 @@ class ViewController: UIViewController {
 //                }
 //            }
 //        }
+        
+        // MARK: Method 4 - Modification using queues
+        downloadImageAndMetadataWithQueue2(imageNumber: 1) { imageDetail, error in
+            DispatchQueue.main.async {
+                if let imageDetail = imageDetail {
+                    print(imageDetail)
+                    print(imageDetail.metadata.name)
+                    self.imageView.image = imageDetail.image
+                    self.metadata.text =  "\(imageDetail.metadata.name) (\(imageDetail.metadata.firstAppearance) - \(imageDetail.metadata.year))"
+                }
+            }
+        }
+        
+        
+        
+        
+    }
+    
+}
+
+
+
+// MARK: Extension for sync method
+
+extension URLSession{
+    /**
+            Este método emplea los métodos asíncronos y un semaforo para simular una llamada http sincrona
+     */
+    func syncDataTask(url: URL) -> (Data?, URLResponse?, Error?){
+        
+        var data: Data?
+        var response: URLResponse?
+        var error : Error?
+        
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        let task = URLSession.shared.dataTask(with: url) {
+            responseData,requestResponse,responseError in
+
+            data = responseData
+            response = requestResponse
+            error = responseError
+            
+            semaphore.signal()
+        }
+        task.resume()
+        _ = semaphore.wait(timeout: .distantFuture)
+        
+        return (data, response, error)
     }
     
 }
